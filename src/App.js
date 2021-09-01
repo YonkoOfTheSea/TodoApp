d8bab4b0e7370efaddd97fd687908e4d7a3c8311
Aman Rathore <amansinghrathore78@gmail.com>
2021-09-02 00:40:28 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
 import TodoList from './components/TodoList';
+import Login from "./components/login";
+
 
 function App() {
   return (
     <div className='todo-app'>
-      <TodoList />
-    </div>
+      <Login data="something" newData = "new something"></Login>   
+   </div>
   );
 }
 
---
+++ src/config/firebase.js
@@ -0,0 +1,16 @@
+import firebase from '@firebase/app'
+import 'firebase/auth'
+
+var firebaseConfig = {
+    apiKey: "AIzaSyAAW2XdJGxIBDjkX89WOr88QtjLYbKUv_s",
+    authDomain: "todo-app-259d4.firebaseapp.com",
+    projectId: "todo-app-259d4",
+    storageBucket: "todo-app-259d4.appspot.com",
+    messagingSenderId: "1084283039625",
+    appId: "1:1084283039625:web:04dc1c2f312d7bf720bb58"
+  };
+
+let firebaseApp = firebase.initializeApp(firebaseConfig);
+let firebaseAuth = firebaseApp.auth();
+
+export default firebaseAuth;
